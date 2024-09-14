7c2836d73d9725c5aff13efe02d0c9c6f8a19369
pman <pman-$-432894328923@gmail.com>
2024-09-14 11:40:15 +0200

    ...

--- clusters/my-cluster/sops/encrypt.sh
+++ clusters/my-cluster/sops/encrypt.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+
 # generate age key
 rm -i age.agekey
 age-keygen -o age.agekey
+
 
 # create decryption secret
 kubectl delete secret sops-age -n flux-system
 kubectl create secret generic sops-age \
     --namespace=flux-system \
     --from-file=age.agekey
+
 
 # generate secret yaml
 cat <<EOF> basic-auth.yaml
@@ -20,8 +23,16 @@ metadata:
   name: mysecret
 EOF
 
+
 # encrypt secret yaml
 sops --age="$(grep "public key" age.agekey | awk '{print $NF}')" \
     --encrypt \
     --encrypted-regex '^(data|stringData)$' \
     --in-place basic-auth.yaml
+
+
+# check if decryption works
+echo "Verifying decryption..."
+export SOPS_AGE_KEY_FILE=age.agekey
+sops -d basic-auth.yaml
+
